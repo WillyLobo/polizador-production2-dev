fe1a49fec1282f6266907e4ad2ed00eb9765549e
WillyLobo <willylobo@gmail.com>
2024-09-03 09:57:47 -0300

    Fixed "home" url that was missing login_required

--- polizador/polizador/urls.py
+++ polizador/polizador/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path("", auth_views.LoginView.as_view(redirect_authenticated_user=True, redirect_field_name="polizas")),
@@ -15,7 +16,7 @@ urlpatterns = [
     #path("api/", include("api.urls")),
     path('admin/', admin.site.urls),
     path("select2/", include("django_select2.urls")),
-    path("home/", TemplateView.as_view(template_name='index.html'), name="home")
+    path("home/", login_required(TemplateView.as_view(template_name='index.html')), name="home")
 ]
 debugpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
