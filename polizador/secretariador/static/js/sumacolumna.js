422f7cdd976f3609cce87233e089893fa635e79d
WillyLobo <willylobo@gmail.com>
2025-07-24 10:12:18 -0300

    Se añadió la función getCUIT para calcular el CUIT a partir del DNI y género. Se implementó un script en las plantillas de creación y actualización de comisionados para mostrar el CUIT calculado al cambiar el DNI. También se agregó un elemento en la barra lateral para mostrar el CUIT calculado.

--- polizador/secretariador/static/js/sumacolumna.js
+++ polizador/secretariador/static/js/sumacolumna.js
@@ -132,3 +132,43 @@ function makeGraph(table, rubro, financiamiento, fecha, pesos, acumulado) {
     }
   });
 }
+/**
+ * Given a DNI and a gender it returns the corresponding CUIT.
+ * @param {String} dni
+ * @param {String} gender
+ */
+const getCUIT = (dni, gender = 'M') => {
+  if (!dni || dni.length !== 8) {
+    throw new Error('The DNI number must contain 8 numbers');
+  }
+
+  let genderNumber = gender === 'M' ? 20 : 27;
+
+  const generateDigitVerificator = () => {
+    const multipliers = [2, 3, 4, 5, 6, 7];
+    const genderNumberAndDNI = `${genderNumber}${dni}`;
+
+    let total = 0;
+    let multipliersIndex = 0;
+    for (let i = String(genderNumberAndDNI).length - 1; i > -1; i--) {
+      const sum = genderNumberAndDNI[i] * multipliers[multipliersIndex];
+      total += sum;
+      if (multipliersIndex === 5) multipliersIndex = 0;
+      else multipliersIndex += 1;
+    }
+
+    const digitVerificator = 11 - (total % 11);
+
+    if (digitVerificator === 10) {
+      genderNumber = 23;
+      return generateDigitVerificator();
+    }
+    if (digitVerificator === 11) return 0;
+    return digitVerificator;
+  };
+
+  const digitVerificator = generateDigitVerificator();
+
+  return `${genderNumber}-${dni}-${digitVerificator}`;
+};
+// https://es.wikipedia.org/wiki/Clave_%C3%9Anica_de_Identificaci%C3%B3n_Tributaria
